d0068144be99b35d8ea63e7a0c09d86237b4e040
namberos39-cyber <namberos39@gmail.com>
2025-11-16 23:36:47 +0100

    Update Schedules.tsx

--- src/components/Schedules/Schedules.tsx
+++ src/components/Schedules/Schedules.tsx
@@ -1,9 +1,12 @@
+// import { faMale } from "@fortawesome/free-solid-svg-icons"; // <-- Eliminado
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // <-- Eliminado
 import Status, { StatusType } from "components/status";
 import Line from "components/Line/Line";
 import { getDepartures } from "data/backend";
 import { useEffect, useState } from "react";
 import { Departure } from "types/departure";
 import { Station } from "types/station";
+// import { getOccupancyType, occupancyColor, OccupancyType } from "./occupancy"; // <-- Eliminado
 import "./Schedules.scss";
 
 type Props = {
@@ -37,38 +40,61 @@ function Schedules({ station }: Props) {
   return (
     <section className="schedules">
       <div className="header">
+        {/* Columna LÍNEA añadida */}
         <div className="line">
-          <div>LÍNIA</div>
+          <div>LÍNEA</div>
         </div>
         <div className="destination">
           <div>DESTINACIÓ</div>
+          {/* <div>Destino</div> <-- Eliminado */}
         </div>
         <div className="time">
           <div>PRÒXIMA EIXIDA</div>
+          {/* <div>Próxima salida</div> <-- Eliminado */}
         </div>
+        {/* Columna Ocupació eliminada
+        <div className="occupancy">
+          <div>Ocupació</div>
+          <div>Ocupación</div>
+        </div>
+        */}
       </div>
 
       {departures.map((departure) => {
-        // --- INICIO DEL PARCHEO (Modo correcto) ---
-        let destinationName = departure.destination;
-        if (destinationName === "TALLER TARONGERS-D") {
-          destinationName = "TARONGERS";
-        }
-        // --- FIN DEL PARCHEO ---
+        // const occupancy = getOccupancyType(departure.occupancy); // <-- Eliminado
 
         return (
           <div
             key={`${departure.line}-${departure.destination}-${departure.time}`}
             className="departure"
           >
+            {/* Columna LÍNEA añadida */}
             <div className="line">
               <Line id={departure.line} />
             </div>
             <div className="destination">
-              {/* Mostramos la variable corregida */}
-              <div>{destinationName}</div>
+              {/* <Line id={departure.line} /> <-- Movido a su propia columna */}
+              <div>{departure.destination}</div>
             </div>
             <div className="time">{getDepartureTime(departure.time)}</div>
+            {/* Columna Ocupació eliminada
+            <div className="occupancy">
+              {occupancy === OccupancyType.Unknown ||
+                [...Array(3).keys()].map((key) => {
+                  return (
+                    <FontAwesomeIcon
+                      key={key}
+                      icon={faMale}
+                      color={
+                        key < occupancy
+                          ? occupancyColor.get(occupancy)
+                          : "#cbd0d8"
+                      }
+                    />
+                  );
+                })}
+            </div>
+            */}
           </div>
         );
       })}
@@ -84,6 +110,7 @@ function getDepartureTime(total: number) {
     return (
       <div className="next">
         <div>immediata</div>
+        {/* <div>inmediata</div> <-- Eliminado */}
       </div>
     );
   }
