c0f43b2250bdc2fc31fe81f6874f8e142497fefd
namberos39-cyber <namberos39@gmail.com>
2025-11-16 22:52:48 +0100

    Update DateTime.tsx

--- src/components/DateTime/DateTime.tsx
+++ src/components/DateTime/DateTime.tsx
@@ -3,20 +3,22 @@ import "./DateTime.scss";
 
 function DateTime() {
   const [date, setDate] = useState<Date>(new Date());
-  const [language, setLanguage] = useState<string>("ca");
+  // const [language, setLanguage] = useState<string>("ca"); // <-- Eliminado
 
   useEffect(() => {
     const timeInterval = setInterval(() => {
       setDate(new Date());
     }, 1000);
 
+    /* El intervalo de fecha e idioma se ha eliminado
     const dateInterval = setInterval(() => {
       setLanguage((previous) => (previous === "ca" ? "es" : "ca"));
     }, 5000);
+    */
 
     return () => {
       clearInterval(timeInterval);
-      clearInterval(dateInterval);
+      // clearInterval(dateInterval); // <-- Eliminado
     };
   }, []);
 
@@ -32,6 +34,7 @@ function DateTime() {
             :{date.getSeconds().toLocaleString([], { minimumIntegerDigits: 2 })}
           </small>
         </div>
+        {/* La fecha se ha eliminado
         <div className="date">
           {date.toLocaleDateString(language, {
             weekday: "long",
@@ -39,6 +42,7 @@ function DateTime() {
             day: "numeric",
           })}
         </div>
+        */}
       </div>
     </div>
   );
